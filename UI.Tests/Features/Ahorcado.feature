6d558cbd1e44a65062abb74591a43dae41924451
Camila <camvives@gmail.com>
2022-08-04 22:34:54 -0300

    Init AT test con Specflow

---
+++ UI.Tests/Features/Ahorcado.feature
@@ -0,0 +1,16 @@
+﻿Feature: Juego_Ahorcado
+		In order to play ahorcado
+	As a user of the website
+	I want to know if I won or lost
+
+@partida
+Scenario: Player wins the game letter by letter
+	Given The word to guess 'AGILES'
+	When Player entered A as the LetraIntresada
+	And Player entered G as the LetraIntresada
+	And Player entered I as the LetraIntresada
+	And Player entered L as the LetraIntresada
+	And Player entered E as the LetraIntresada
+	And Player entered S as the LetraIntresada
+	Then Player should be told that he lost the game
+
---
+++ UI.Tests/Steps/Juego_AhorcadoSteps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace UI.Tests.Steps
+{
+    [Binding]
+    public class Juego_AhorcadoSteps
+    {
+        [Given(@"The word to guess '(.*)'")]
+        public void GivenTheWordToGuess(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Player entered A as the LetraIntresada")]
+        public void WhenPlayerEnteredAAsTheLetraIntresada()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Player entered G as the LetraIntresada")]
+        public void WhenPlayerEnteredGAsTheLetraIntresada()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Player entered I as the LetraIntresada")]
+        public void WhenPlayerEnteredIAsTheLetraIntresada()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Player entered L as the LetraIntresada")]
+        public void WhenPlayerEnteredLAsTheLetraIntresada()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Player entered E as the LetraIntresada")]
+        public void WhenPlayerEnteredEAsTheLetraIntresada()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"Player entered S as the LetraIntresada")]
+        public void WhenPlayerEnteredSAsTheLetraIntresada()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"Player should be told that he lost the game")]
+        public void ThenPlayerShouldBeToldThatHeLostTheGame()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
