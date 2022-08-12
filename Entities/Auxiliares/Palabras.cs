b478e4168fb520dfd91cba4c8fd41d195a63c6cd
FranciscoLabanca <labancafrancisco@gmail.com>
2022-08-12 17:00:55 -0300

    Creacion de Clase Palabras

---
+++ Entities/Auxiliares/Palabras.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ahorcado.Entities.Auxiliares
+{
+    public static class Palabras
+    {
+        private static readonly string[] palabras =
+        {
+            "AGILES",
+            "AHORCADO",
+            "MESA",
+            "CASA"
+        };
+
+        public static string PalabraAleatoria()
+        {
+            Random rnd = new Random();
+            int nroAleatorio = rnd.Next(palabras.Length);
+            return palabras[nroAleatorio];
+        }
+    }
+}
